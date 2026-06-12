3425d68eed74b80643e9377c2875953d2141c707
Ross Golder <ross@golder.org>
2026-06-12 16:22:49 +0700

    fix: add missing closing paren for nolint

--- internal/controller/alert/alert.go
+++ internal/controller/alert/alert.go
@@ -66,6 +66,7 @@ func Setup(mgr ctrl.Manager, o controller.Options) error {
 		managed.WithLogger(o.Logger.WithValues("controller", name)),
 		managed.WithPollInterval(o.PollInterval),
 		managed.WithRecorder(event.NewAPIRecorder(mgr.GetEventRecorderFor(name))), //nolint:staticcheck
+	)
 
 	return ctrl.NewControllerManagedBy(mgr).
 		Named(name).
--- internal/controller/dashboard/dashboard.go
+++ internal/controller/dashboard/dashboard.go
@@ -62,6 +62,7 @@ func Setup(mgr ctrl.Manager, o controller.Options) error {
 		managed.WithLogger(o.Logger.WithValues("controller", name)),
 		managed.WithPollInterval(o.PollInterval),
 		managed.WithRecorder(event.NewAPIRecorder(mgr.GetEventRecorderFor(name))), //nolint:staticcheck
+	)
 
 	return ctrl.NewControllerManagedBy(mgr).
 		Named(name).
